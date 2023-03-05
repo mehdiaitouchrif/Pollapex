9b9548cef097caf809ff5cdf0a00cd81458aa0c9
mehdiaitouchrif <mehdi.aitouchrif@outlook.com>
2023-03-05 16:43:24 +0100

    delete extended error class and add success attribute to input errors

--- api/middleware/error.js
+++ api/middleware/error.js
@@ -4,7 +4,7 @@ const handleInputErrors = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
+    res.status(400).json({ success: false, errors: errors.array() });
   } else {
     next();
   }
