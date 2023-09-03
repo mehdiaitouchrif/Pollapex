a32cef48dfbeb167a413e2d7514529f85b6f85cf
Mehdi Ait Ouchrif <mehdi.aitouchrif@outlook.com>
2023-09-04 00:29:04 +0100

    make it more flexible

--- client/app/components/skeleton.js
+++ client/app/components/skeleton.js
@@ -1,30 +1,18 @@
-const SkeletonBox = () => {
+const SkeletonBox = ({ styleClasses, rowNumber = 3 }) => {
   return (
     <div
       role='status'
-      className='p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700'
+      className={`p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700 ${styleClasses}`}
     >
-      <div className='flex items-center justify-between'>
-        <div>
-          <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 mb-2.5'></div>
-          <div className='w-72 h-2 bg-gray-200 rounded-full dark:bg-gray-600'></div>
+      {Array.from({ length: rowNumber }, (_, index) => index + 1).map((row) => (
+        <div key={row} className='flex items-center justify-between'>
+          <div>
+            <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 mb-2.5'></div>
+            <div className='w-72 h-2 bg-gray-200 rounded-full dark:bg-gray-600'></div>
+          </div>
+          <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-12'></div>
         </div>
-        <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-12'></div>
-      </div>
-      <div className='flex items-center justify-between pt-4'>
-        <div>
-          <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 mb-2.5'></div>
-          <div className='w-72 h-2 bg-gray-200 rounded-full dark:bg-gray-700'></div>
-        </div>
-        <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-12'></div>
-      </div>
-      <div className='flex items-center justify-between pt-4'>
-        <div>
-          <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600  mb-2.5'></div>
-          <div className='w-72 h-2 bg-gray-200 rounded-full dark:bg-gray-700'></div>
-        </div>
-        <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-12'></div>
-      </div>
+      ))}
 
       <span className='sr-only'>Loading...</span>
     </div>
