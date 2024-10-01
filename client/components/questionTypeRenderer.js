1975beef8986f4d97126fe065aba832e009b3a3b
Mehdi Ait Ouchrif <mehdi.aitouchrif@outlook.com>
2024-10-01 14:51:57 +0100

    add phone & time in survey form

--- client/components/questionTypeRenderer.js
+++ client/components/questionTypeRenderer.js
@@ -14,19 +14,19 @@ const QuestionTypeRenderer = ({
     case "phone":
     case "email":
       return (
-        <div className='relative z-0 w-full mb-6 group'>
+        <div className="relative z-0 w-full mb-6 group">
           <input
-            type='text'
-            name='title'
+            type="text"
+            name="title"
             disabled
-            id='title'
-            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-            placeholder=' '
+            id="title"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
             required
           />
           <label
-            htmlFor='title'
-            className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+            htmlFor="title"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             {camelCaseToTitleCase(question.type)} answer type
           </label>
@@ -37,19 +37,19 @@ const QuestionTypeRenderer = ({
       return (
         <div>
           {question.choices.map((op, idx) => (
-            <div key={idx} className='flex gap-4 items-center my-4'>
+            <div key={idx} className="flex gap-4 items-center my-4">
               <input
                 disabled
-                type='checkbox'
+                type="checkbox"
                 checked={false}
-                className='w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+                className="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
               />
-              <div className='relative z-0 w-full group mb-3 flex items-center gap-4'>
+              <div className="relative z-0 w-full group mb-3 flex items-center gap-4">
                 <input
-                  type='text'
-                  name='title'
-                  id='title'
-                  className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                  type="text"
+                  name="title"
+                  id="title"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   value={op}
                   onChange={(e) =>
                     handleChoiceChange(index, idx, e.target.value)
@@ -58,7 +58,7 @@ const QuestionTypeRenderer = ({
                 />
                 <AiOutlineCloseCircle
                   size={24}
-                  className='cursor-pointer hover:text-red-700 mt-4'
+                  className="cursor-pointer hover:text-red-700 mt-4"
                   onClick={() => deleteChoice(index, idx)}
                 />
               </div>
@@ -66,7 +66,7 @@ const QuestionTypeRenderer = ({
           ))}
 
           <button
-            className='bg-black text-white rounded shadow py-2 px-4 my-4'
+            className="bg-black text-white rounded shadow py-2 px-4 my-4"
             onClick={() => addChoice(index)}
           >
             Add option
@@ -74,25 +74,35 @@ const QuestionTypeRenderer = ({
         </div>
       );
     case "time":
+      return (
+        <div className="my-4">
+          <input
+            type="time"
+            id="time"
+            disabled
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block md:w-1/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+      );
     case "date":
       return (
-        <div className='my-4'>
+        <div className="my-4">
           <input
-            type='date'
-            id='date'
+            type="date"
+            id="date"
             disabled
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block md:w-1/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            placeholder='Month, day, year'
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block md:w-1/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Month, day, year"
           />
         </div>
       );
     case "rating":
       return (
-        <div className='flex items-center justify-center gap-4 my-4'>
+        <div className="flex items-center justify-center gap-4 my-4">
           {Array.from({ length: 5 }, (_, index) => (
             <div
               key={index}
-              className='h-10 w-10 rounded-full border-black border-2 flex items-center justify-center text-center hover:bg-gray-50 cursor-not-allowed'
+              className="h-10 w-10 rounded-full border-black border-2 flex items-center justify-center text-center hover:bg-gray-50 cursor-not-allowed"
             >
               {index + 1}
             </div>
--- client/components/surveyForm.js
+++ client/components/surveyForm.js
@@ -5,8 +5,8 @@ const SurveyForm = ({ survey, answers, handleInputChange, handleSubmit }) => {
     switch (type) {
       case "multipleChoice":
         return (
-          <div key={_id} className='my-2'>
-            <div className='flex flex-col'>
+          <div key={_id} className="my-2">
+            <div className="flex flex-col">
               {choices.map((choice) => (
                 <label
                   key={choice}
@@ -19,13 +19,13 @@ const SurveyForm = ({ survey, answers, handleInputChange, handleSubmit }) => {
                   }`}
                 >
                   <input
-                    type='checkbox'
-                    className='form-checkbox text-blue-600 h-5 w-5'
+                    type="checkbox"
+                    className="form-checkbox text-blue-600 h-5 w-5"
                     onChange={(e) =>
                       handleInputChange(_id, [...(answers[_id] || []), choice])
                     }
                   />
-                  <span className='ml-2'>{choice}</span>
+                  <span className="ml-2">{choice}</span>
                 </label>
               ))}
             </div>
@@ -34,8 +34,8 @@ const SurveyForm = ({ survey, answers, handleInputChange, handleSubmit }) => {
 
       case "singleChoice":
         return (
-          <div key={_id} className='my-2'>
-            <div className='flex flex-col'>
+          <div key={_id} className="my-2">
+            <div className="flex flex-col">
               {choices.map((choice) => (
                 <label
                   key={choice}
@@ -47,13 +47,13 @@ const SurveyForm = ({ survey, answers, handleInputChange, handleSubmit }) => {
                   }`}
                 >
                   <input
-                    type='radio'
-                    className='form-radio text-blue-600 h-5 w-5'
+                    type="radio"
+                    className="form-radio text-blue-600 h-5 w-5"
                     name={`question_${_id}`}
                     value={choice}
                     onChange={(e) => handleInputChange(_id, choice)}
                   />
-                  <span className='ml-2'>{choice}</span>
+                  <span className="ml-2">{choice}</span>
                 </label>
               ))}
             </div>
@@ -62,29 +62,62 @@ const SurveyForm = ({ survey, answers, handleInputChange, handleSubmit }) => {
 
       case "date":
         return (
-          <div key={_id} className='my-2'>
+          <div key={_id} className="my-2">
             <input
-              type='date'
-              className='mt-2 p-2 border rounded'
-              onChange={(e) => handleInputChange(id, e.target.value)}
+              type="date"
+              className="mt-2 p-2 border rounded"
+              onChange={(e) => handleInputChange(_id, e.target.value)}
             />
+          </div>
+        );
+
+      case "time":
+        return (
+          <div key={_id} className="my-2">
+            <input
+              type="time"
+              id="time"
+              className="mt-2 p-2 border rounded"
+              onChange={(e) => handleInputChange(_id, e.target.value)}
+            />
+          </div>
+        );
+
+      case "phone":
+        return (
+          <div key={_id} className="my-2">
+            <label
+              htmlFor="phone"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Phone Number
+            </label>
+            <div className="mt-1">
+              <input
+                type="tel"
+                id="phone"
+                onChange={(e) => handleInputChange(_id, e.target.value)}
+                placeholder="+1 (123) 456-7890"
+                className="w-full px-2 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-sm"
+              />
+            </div>
           </div>
         );
 
       case "text":
         return (
-          <div key={_id} className='my-2 relative z-0'>
+          <div key={_id} className="my-2 relative z-0">
             <input
-              type='text'
-              className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-              name='text'
-              id='text'
-              placeholder=' '
-              onChange={(e) => handleInputChange(id, e.target.value)}
+              type="text"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              name="text"
+              id="text"
+              placeholder=" "
+              onChange={(e) => handleInputChange(_id, e.target.value)}
             />
             <label
-              htmlFor='text'
-              className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              htmlFor="text"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Short text
             </label>
@@ -93,17 +126,17 @@ const SurveyForm = ({ survey, answers, handleInputChange, handleSubmit }) => {
 
       case "paragraph":
         return (
-          <div key={_id} className='my-2 relative z-0'>
+          <div key={_id} className="my-2 relative z-0">
             <textarea
-              className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-1 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-              placeholder=' '
-              rows='4'
-              id='paragraph'
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-1 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              rows="4"
+              id="paragraph"
               onChange={(e) => handleInputChange(_id, e.target.value)}
             ></textarea>
             <label
-              htmlFor='paragraph'
-              className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              htmlFor="paragraph"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Paragraph
             </label>
@@ -112,10 +145,10 @@ const SurveyForm = ({ survey, answers, handleInputChange, handleSubmit }) => {
 
       case "email":
         return (
-          <div key={_id} className='mb-4'>
+          <div key={_id} className="mb-4">
             <input
-              type='email'
-              className='mt-2 p-2 border rounded w-full'
+              type="email"
+              className="mt-2 p-2 border rounded w-full"
               onChange={(e) => handleInputChange(_id, e.target.value)}
             />
           </div>
@@ -125,10 +158,10 @@ const SurveyForm = ({ survey, answers, handleInputChange, handleSubmit }) => {
         return (
           <div
             key={_id}
-            className='mb-4 flex items-center justify-center md:justify-between'
+            className="mb-4 flex items-center justify-center md:justify-between"
           >
-            <p className='pl-4 hidden md:block'>Negative</p>
-            <div className='mt-2 flex items-center justify-center gap-3'>
+            <p className="pl-4 hidden md:block">Negative</p>
+            <div className="mt-2 flex items-center justify-center gap-3">
               {[1, 2, 3, 4, 5].map((rating) => (
                 <label
                   key={rating}
@@ -140,8 +173,8 @@ const SurveyForm = ({ survey, answers, handleInputChange, handleSubmit }) => {
                   }`}
                 >
                   <input
-                    type='radio'
-                    className='sr-only'
+                    type="radio"
+                    className="sr-only"
                     name={`question_${_id}`}
                     value={rating}
                     checked={
@@ -154,7 +187,7 @@ const SurveyForm = ({ survey, answers, handleInputChange, handleSubmit }) => {
                 </label>
               ))}
             </div>
-            <p className='pr-4 hidden md:block'>Positive</p>
+            <p className="pr-4 hidden md:block">Positive</p>
           </div>
         );
 
@@ -168,21 +201,21 @@ const SurveyForm = ({ survey, answers, handleInputChange, handleSubmit }) => {
       {survey.questions.map((question, idx) => (
         <div
           key={question._id}
-          className='bg-white py-3 rounded-lg shadow-sm border border-gray-200 my-4'
+          className="bg-white py-3 rounded-lg shadow-sm border border-gray-200 my-4"
         >
-          <p className='font-semibold py-2 px-4'>
+          <p className="font-semibold py-2 px-4">
             {idx + 1}. {question.question}{" "}
-            {!question.optional && <span className='text-red-700'>*</span>}
+            {!question.optional && <span className="text-red-700">*</span>}
           </p>
-          <div className='py-2 px-4 border-t border-gray-200 text-sm text-gray-600 font-normal'>
+          <div className="py-2 px-4 border-t border-gray-200 text-sm text-gray-600 font-normal">
             {renderQuestion(question)}
           </div>
         </div>
       ))}
       {survey.questions?.length > 0 && (
         <button
-          type='submit'
-          className='inline-block py-2 px-6 rounded-lg text-white font-medium bg-blue-600 hover:bg-blue-700'
+          type="submit"
+          className="inline-block py-2 px-6 rounded-lg text-white font-medium bg-blue-600 hover:bg-blue-700"
         >
           Submit
         </button>
